03cb86f66f646c7df60bff398d286ec46999ff3b
Zaslavsky <Alex.Zaslavsky@rsa.com>
2018-03-05 11:21:47 -0500

    grunt part1

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+'use strict'
+
+module.exports = function(grunt) {
+
+  require('time-grunt')(grunt);
+
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    sass:{
+      dist:{
+        files:{
+          'css/styles.css':'css/styles.scss'
+        }
+      }
+    },
+
+    watch: {
+      files: 'css/*.scss',
+      tasks: ['sass']
+    },
+
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
+            'css/*.css',
+            '*.html',
+            'js/*.js'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            daseDir: './'
+          }
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask('css',['sass']);
+  grunt.registerTask('default',['browserSync','watch']);
+}
